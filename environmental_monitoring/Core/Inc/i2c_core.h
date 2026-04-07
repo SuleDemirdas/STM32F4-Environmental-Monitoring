163d0c9dd068e517acf595da8289c16f14871018
SuleDemirdas <109147185+SuleDemirdas@users.noreply.github.com>
2026-04-07 18:26:41 +0300

    feat(i2c-core): add I2C device address scanner

--- environmental_monitoring/Core/Inc/i2c_core.h
+++ environmental_monitoring/Core/Inc/i2c_core.h
@@ -9,6 +9,7 @@
 #define INC_I2C_CORE_H_
 
 #include "stdint.h"
+#include "stm32f4xx_hal.h"
 
 typedef enum
 {
@@ -18,5 +19,6 @@ typedef enum
 }sensor_t;
 
 float i2c_sensor_read(uint8_t device_address, sensor_t sensor_type);
+uint8_t I2C_ScanDeviceAddress(void);
 
 #endif /* INC_I2C_CORE_H_ */
